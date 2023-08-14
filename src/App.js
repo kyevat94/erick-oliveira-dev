e2ba4dacbb2b8cc29a8588b0834fbe44f5f596e2
Erick Oliveira <kyevat@gmail.com>
2023-08-14 17:34:09 -0300

    Creation of top bar, header and about me divs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [searchTxt, setSearchTxt] = useState("Search");
+
+  const [boxExplainText, setBoxExplainText] = useState(
+    "Hover/click the languages names to know more about them."
+  );
+
+  const searchOnFocus = (focusState) => {
+    setSearchTxt(focusState);
+  };
+
+  function showLangInfo(lang) {
+    if (lang === "html") {
+      setBoxExplainText("html<hr/>");
+    }
+  }
+
+  function setDefaultLangInfo() {
+    setBoxExplainText(
+      "Hover/click the languages names to know more about them."
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top-bar">
+        <input
+          type="text"
+          placeholder={searchTxt}
+          className="search-top"
+          onFocus={() => searchOnFocus("")}
+          onBlur={() => searchOnFocus("Search")}
+        ></input>
+      </div>
+      <div className="main">
+        <div className="header-div">
+          <div className="capa-header-div"></div>
+          <div className="foto-main"></div>
+          <div className="text-header-div">
+            <h1>Érick Oliveira</h1>
+            <h3>Full-Stack Web Developer</h3>
+          </div>
+          <div className="footer-header-div"></div>
+        </div>
+        <div className="about-me-div">
+          <div className="text-about-me-div">
+            <p>
+              Student of Software Engeneering by Anhanguera College, Math
+              teacher, and Full-stack developer, with knowledge of &nbsp;
+              <span
+                className="lang-box"
+                id="html-box"
+                onMouseOver={() => showLangInfo("html")}
+                onMouseOut={setDefaultLangInfo}
+              >
+                HTML
+              </span>
+              , &nbsp;
+              <span className="lang-box" id="css-box">
+                CSS
+              </span>
+              , &nbsp;
+              <span className="lang-box" id="js-box">
+                JavaScript
+              </span>
+              , &nbsp;
+              <span className="lang-box" id="ts-box">
+                TypeScript
+              </span>
+              , &nbsp;
+              <span className="lang-box" id="react-box">
+                React
+              </span>
+              , and &nbsp;
+              <span className="lang-box" id="py-box">
+                Python
+              </span>
+              .
+            </p>
+            <hr />
+            <div
+              id="box-explain"
+              className="box-default"
+              dangerouslySetInnerHTML={{ __html: boxExplainText }}
+            ></div>
+          </div>
+          <div className="footer-header-div"></div>
+        </div>
+      </div>
     </div>
   );
 }
